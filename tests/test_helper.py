1748f08a640045bcc5209b79731d559e816d1d31
Jabba Laci <jabba.laci@gmail.com>
2018-07-05 14:35:56 +0200

    tests

--- tests/test_helper.py
+++ tests/test_helper.py
@@ -6,6 +6,10 @@ def test_fold_urls():
     res = h.fold_urls(lst)
     assert res == "node[1-3]"
 
+    lst = ["node1", "node5", "node9"]
+    res = h.fold_urls(lst)
+    assert res == "node[1,5,9]"
+
     lst = ["node01", "node02", "node03"]
     res = h.fold_urls(lst)
     assert res == "node[01-03]"
@@ -13,6 +17,14 @@ def test_fold_urls():
     lst = ["http://www.website.com/005.jpg", "http://www.website.com/006.jpg", "http://www.website.com/007.jpg"]
     res = h.fold_urls(lst)
     assert res == "http://www.website.com/[005-007].jpg"
+
+    lst = ["aa", "bb", "cc"]
+    res = h.fold_urls(lst)
+    assert res == "aa,bb,cc"
+
+    lst = ["X01", "X01", "X01"]
+    res = h.fold_urls(lst)
+    assert res == "X01"    # no duplicates
 
 
 def test_unfold_sequence_url():
